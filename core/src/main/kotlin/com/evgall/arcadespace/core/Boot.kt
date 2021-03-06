5cd861acb262a3b75c603983b93ec38e0738c233
AlexandrovEugen <evgeniy.alexandrov@gmail.com>
2021-03-06 13:26:38 +0300

    Added ktx extension for game pattern and logging

--- core/src/main/kotlin/com/evgall/arcadespace/core/Boot.kt
+++ core/src/main/kotlin/com/evgall/arcadespace/core/Boot.kt
@@ -1,11 +1,20 @@
 package com.evgall.arcadespace.core
 
-import com.badlogic.gdx.Game
-import com.evgall.arcadespace.core.FirstScreen
+import ktx.app.KtxGame
+import ktx.app.KtxScreen
+import ktx.log.Logger
+import ktx.log.debug
+import ktx.log.info
+import ktx.log.logger
+
+
+private val LOG: Logger = logger<Boot>()
 
 /** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms.  */
-class Boot : Game() {
+class Boot : KtxGame<KtxScreen>() {
     override fun create() {
-        setScreen(FirstScreen())
+        LOG.debug { "Create game instance" }
+        addScreen(FirstScreen())
+        setScreen<FirstScreen>()
     }
 }
